80952f1a3e562a9e5b0c866cac13f570de5adbf4
kptudyogamela <kptudyogamela@gmail.com>
2025-06-04 17:19:23 +0530

    after changing images in first page

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 import React from "react";
 import ProductsClient from "./ProductsClient";
 
 export default function ProductsPage() {
   return (
-    <main>
+    <main className="p-4 md:p-8 bg-gray-50 min-h-screen">
       <ProductsClient />
     </main>
   );
